ef1276a1f03aabbe65aede0a99438e022bc1bfb0
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-08-05 14:34:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
   resources :authors, only: [:show] do
-  resources :posts, only: [:index, :show]
+    # nested resource for posts
+    resources :posts, only: [:show, :index]
   end
-
-resources :posts only: [:index, :show, :update, :edit, :create, :new]
+ 
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+ 
   root 'posts#index'
-
 end
