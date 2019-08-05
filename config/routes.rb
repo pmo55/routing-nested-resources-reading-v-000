e52809df404fc2a7b82e0324499ed6b18ff59f51
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-08-05 14:21:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :authors, only: [:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  get 'authors/:id/posts'
+  get 'authors/:id/posts', to 'authors/'
   get 'authors/:id/posts/:post_id'
 
 
